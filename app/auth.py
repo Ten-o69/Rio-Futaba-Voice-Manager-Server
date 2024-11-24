48e815c31222ffb0cfc61349a60de586f5fda9f2
Ten-o69 <03822007sasha@gmail.com>
2024-11-25 01:23:51 +0300

    The auth module has been updated with functionality for adding a "refresh token" to the database.

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone, timedelta
+from uuid import uuid4
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
-from pydantic import ValidationError
 
 from common.constants import (
     JWT_SECRET_KEY,
@@ -18,18 +18,29 @@ def create_access_token(data: dict) -> str:
     Создаёт короткоживущий Access токен для устройства.
     """
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES)  # 15 минут
+    expire = datetime.now(timezone.utc) + timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire, "type": "access"})
 
     return jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
-def create_refresh_token(data: dict) -> str:
+def create_refresh_token(db: Session, data: dict) -> str:
     """
-    Создаёт Refresh токен для устройства.
+    Создаёт Refresh токен для устройства и добавляем его в таблицу Token.
     """
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(days=JWT_REFRESH_TOKEN_EXPIRE_DAYS)  # 30 дней
     to_encode.update({"exp": expire, "type": "refresh"})
+    jti = str(uuid4()) + str(expire) + str(uuid4())
+
+    create_token = Token(
+        jti=jti,
+        device_id=data["id"],
+        expires_at=expire,
+    )
+
+    db.add(create_token)
+    db.commit()
+    db.refresh(create_token)
 
     return jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
@@ -69,7 +80,7 @@ def verify_refresh_token(token: str, db: Session) -> dict | None:
     payload = verify_token(token, expected_type="refresh")
 
     if payload:
-        db_token = db.query(Token).filter(Token.token == token).first()
+        db_token = db.query(Token).filter(Token.jti == payload["jti"]).first()
 
         if not db_token:
             raise JWTError("Token not found in database")
