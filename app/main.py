d78e2f2253a4ba90075732a7ad80def46b13a217
Ten-o69 <03822007sasha@gmail.com>
2024-11-25 01:24:54 +0300

    Added functionality to run the uvicorn server at the project entry point

--- app/main.py
+++ app/main.py
@@ -4,8 +4,13 @@ from app.routes import device, file
 app = FastAPI(title="Voice Manager API")
 
 # Подключаем роуты
-
+app.include_router(device.router, prefix="/devices", tags=["Devices"])
 
 @app.get("/")
 async def root():
     return {"message": "Voice Manager Server is running"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
