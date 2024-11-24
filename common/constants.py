997c164ee5fca7bc579798f6f287d981117999f5
Ten-o69 <03822007sasha@gmail.com>
2024-11-25 01:17:10 +0300

    fix type var in constants.py

--- common/constants.py
+++ common/constants.py
@@ -1,11 +1,12 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 # paths
-CDIR = os.getcwd() + "/"
-PATH_CONFIG_MODULE = os.path.join(CDIR, "config")
+PROJECT_ROOT = Path(__file__).parent.parent
+PATH_CONFIG_MODULE = os.path.join(PROJECT_ROOT, "config")
 PATH_SECRETS_FILE = os.path.join(PATH_CONFIG_MODULE, "secrets.env")
-PATH_UPLOAD_DIR = os.path.join(CDIR, "../files")
+PATH_UPLOAD_DIR = os.path.join(PROJECT_ROOT, "files")
 
 load_dotenv(PATH_SECRETS_FILE)
 
@@ -15,5 +16,5 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # jwt
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")  # Секретный ключ
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")  # Алгоритм подписи токенов
-JWT_ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES")  # Время жизни access-токена (в минутах)
-JWT_REFRESH_TOKEN_EXPIRE_DAYS = os.getenv("JWT_REFRESH_TOKEN_EXPIRE_DAYS")  # Время жизни refresh-токена (в днях)
+JWT_ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES"))  # Время жизни access-токена (в минутах)
+JWT_REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("JWT_REFRESH_TOKEN_EXPIRE_DAYS"))  # Время жизни refresh-токена (в днях)
